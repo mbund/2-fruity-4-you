ed76dd6bdf1b91e37d708b2b1d217c040d15a759
mbund <mbund@users.noreply.github.com>
2022-11-29 05:56:31 +0000

    deploy: 6f9f5c00b80f088285296cc1c180a1decf2290c9

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['endgame_0',['EndGame',['../classEndGame.html#a716a438b0f11a3efe08352c30873a12d',1,'EndGame']]]
+  ['end_0',['end',['../classEndGame.html#a2f51af756abab0fa5a59345dc04442e8',1,'EndGame']]],
+  ['endgame_1',['EndGame',['../classEndGame.html#a716a438b0f11a3efe08352c30873a12d',1,'EndGame']]]
 ];
