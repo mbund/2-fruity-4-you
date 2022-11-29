381d68982295be37c5f204a2d91001d4d7637c9a
Mark Bundschuh <mark@mbund.org>
2022-11-29 15:58:20 -0500

    update nix

--- flake.nix
+++ flake.nix
@@ -4,8 +4,12 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.nixpkgs-doxygen.url = "github:NixOS/nixpkgs/cef50cde2bd337c795159e8b26c7c246206b6740";
+  inputs.feh-proteus-simulator-libraries = {
+    url = "git+https://code.osu.edu/fehelectronics/proteus_software/simulator_libraries.git";
+    flake = false;
+  };
 
-  outputs = { self, flake-utils, nixpkgs, nixpkgs-doxygen }:
+  outputs = { self, flake-utils, nixpkgs, nixpkgs-doxygen, feh-proteus-simulator-libraries }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -33,6 +37,9 @@
         packages.default = pkgs.stdenv.mkDerivation rec {
           inherit name buildInputs nativeBuildInputs;
           src = ./.;
+          patchPhase = ''
+            cp -r ${feh-proteus-simulator-libraries} vendor/simulator-libraries
+          '';
           installPhase = ''
             mkdir -p $out/bin
             cp game.out $out/bin/${name}
